510243068980e950e9bcbd2bda87afdd1db439eb
yama7321 <ryota.yamaoka7321@gmail.com>
2020-07-24 18:55:52 +0900

    ユーザー保存機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+  root 'products#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 end
