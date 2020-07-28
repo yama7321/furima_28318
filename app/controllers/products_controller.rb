0558c89b587696138694123f6e25fb5f125ce430
yama7321 <ryota.yamaoka7321@gmail.com>
2020-07-28 19:48:46 +0900

    Active Storage の導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
   def index
   end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:image, :name, :description, :category, :status, :shipping_cost, :prefecture_id, :shipping_days, :price, :user, :sold_out).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  
+  has_one_attached :image
 end
