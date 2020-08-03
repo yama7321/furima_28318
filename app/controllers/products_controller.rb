a4c71aa4b2587668b50bc8a74fc9fc61d20e6e0f
yama7321 <ryota.yamaoka7321@gmail.com>
2020-08-03 12:27:15 +0900

    updateアクションにset_productメソッドを適用

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -28,7 +28,6 @@ class ProductsController < ApplicationController
   end
   
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to root_path
     else
