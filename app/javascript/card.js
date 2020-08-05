61a1396bb15ddf9da34d0e47daf14b7c85f74872
yama7321 <ryota.yamaoka7321@gmail.com>
2020-08-05 14:49:19 +0900

    console2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,8 +14,10 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-
+    console.log(card);
     Payjp.createToken(card, (status, response) => {
+      console.log(status);
+      cosole.log(response);
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
